e705b727501d04e4a05e9a33458bc68bad77d81d
TheWisePigeon <zozozozeph@gmail.com>
2022-10-05 20:14:59 +0000

    feat: added login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
-<script></script>
+<script>
+	import { redirect } from '@sveltejs/kit';
+	import { onMount } from 'svelte';
+	import { db, app } from '../FirebaseClient';
+
+	let loggedIn;
+
+	onMount(() => {
+		if (window != undefined) {
+			window.location.href = '/login'
+		}
+	});
+</script>
 
 <div class=" flex justify-center bg-green-500 p-6 font-extrabold text-7xl title">
-    Welcome to LOLAND
+	<h1>Welcome to LOLAND</h1>
 </div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,6 +2,24 @@
 
 </script>
 
-<div class=" flex justify-center items-center h-screen bg-green-500 p-6 font-extrabold text-7xl title" >
-    Welcome to LOLAND
+<div class=" flex flex-col justify-center items-center h-screen bg-green-500 p-6 font-extrabold text-5xl title" >
+    <div class=" flex flex-col text-center">
+        <div>
+            Welcome to LOLAND BRUH
+        </div>
+        <div>
+            Before we let you in, we must know who you are
+        </div>
+    </div>
+    <br>
+    <div class=" flex flex-col gap-10">
+        <input 
+        type="text" 
+        placeholder="bruh or something" 
+        name="" id="" 
+        class="rounded-xl text-center p-2 border-2 border-green-800 bg-green-100 caret-green-700 text-green-900 focus:bg-green-200 placeholder:text-slate-300 focus:placeholder:text-slate-400 focus:outline-none" >
+        <button class=" rounded-xl bg-green-600 text-green-900 hover:bg-green-700 hover:text-green-200 border-2  border-green-700 hover:border-green-800 hover:scale-105 p-2 transition-all">
+            Enter the LOLAND
+        </button>
+    </div>
 </div>
